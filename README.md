4ed9f0c27f0acc6929d8fbdf47a19c126903835d
Wilson <wilson_lou@hotmail.co.uk>
2018-04-28 12:34:50 -0600

    finish ex

--- README.md
+++ README.md
@@ -2,4 +2,9 @@
 
 ## Week 1 Homework for Skill Distillery
 
-Add in objective, how it works, what tech used
+How it works:
+	1. Program asks for the price of an item and money tendered via a scanner
+	2. If price is equal to money then no change is required and programs stops
+	3. If price > money then user is prompted to enter more money until this condition is met via a do while loop
+	4. If price < money then exact change is given as a count of each denomination via notes and coins. Only counts which are !=0 are shown. 
+	
--- src/com/skilldistillery/makechange/MakeChange.java
+++ src/com/skilldistillery/makechange/MakeChange.java
@@ -5,71 +5,90 @@ import java.util.Scanner;
 public class MakeChange {
 
 	public void run() {
+		//ask for price of item and money tendered
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter price of item");
 		double price = sc.nextDouble();
-		System.out.println("Price of item is: " + price);
+		System.out.print("Price of item is: ");
+		System.out.printf("%.2f", price);
+		System.out.println();
 		System.out.println("Money tendered by the customer?");
 		double money = sc.nextDouble();
+		
+		//calculate change required or ask for more money
 		double exactChange = money - price;
 		if (exactChange == 0) {
-			System.out.println("Exact change given");
+			System.out.println("Exact change given. Ta!");
 		} else if (price < money) {
+			//call on calculate change method
 			provideChange(exactChange, money, price);
 		} else {
+			//keep asking for more money until greater than price
 			do {
-				System.out.println(
-						"Item is more than money tendered, please ask for more money and re-enter amount tendered");
+				System.out.print("The price of ");
+				System.out.printf("%.2f", price);
+				System.out.print (" is more than ");
+				System.out.printf("%.2f", money);
+				System.out.println(" tendered");
+				System.out.print("Please re-enter money greater than ");
+				System.out.printf("%.2f", price);
 				money = sc.nextDouble();
 				exactChange = money - price;
 			} while (price > money);
+			//call on calculate change method once money tendered is greater than price
 			provideChange(exactChange, money, price);
 		}
 	}
 
-	private void provideChange(double exactChange, double money, double price) {
+	public void provideChange(double exactChange, double money, double price) {
+		//calculate change
 		int change2 = (int) (Math.round((money - price) * 100));
+		//display how much change required
 		System.out.print("Change will be provided: ");
 		System.out.printf("%.2f", exactChange);
 		System.out.println();
 
+		//divide change by denominations and use int to truncate
+		//take the modulus and assign that to new change value
 		int twenty = (int) (change2 / 2000);
 		int change3 = (int) (change2 % 2000);
-		System.out.println("20:" + twenty);
 
 		int ten = (int) (change3 / 1000);
 		int change4 = (int) (change3 % 1000);
-		System.out.println("10:" + ten);
 
 		int five = (int) (change4 / 500);
 		int change5 = (int) (change4 % 500);
-		System.out.println("5:" + five);
 
 		int two = (int) (change5 / 200);
 		int change6 = (int) (change5 % 200);
-		System.out.println("2:" + two);
 
 		int one = (int) (change6 / 100);
 		int change7 = (int) (change6 % 100);
-		System.out.println("1:" + one);
 
 		int half = (int) (change7 / 50);
 		int change8 = (int) (change7 % 50);
-		System.out.println("0.5:" + half);
 
 		int quarter = (int) (change8 / 25);
 		int change9 = (int) (change8 % 25);
-		System.out.println("0.25:" + quarter);
 
 		int dime = (int) (change9 / 10);
 		int change10 = (int) (change9 % 10);
-		System.out.println("0.10: " + dime);
 
 		int nickel = (int) (change10 / 5);
 		int change11 = (int) (change10 % 5);
-		System.out.println("0.05: " + nickel);
 
 		int penny = (int) (change11 / 1);
-		System.out.println("0.01: " + penny);
+
+		int denominations[] = { twenty, ten, five, two, one, half, quarter, dime, nickel, penny };
+		String denoms[] = { "Twenties", "Tens", "Fives", "Twos", "Ones", "Halves", "Quarters", "Dimes", "Nickels",
+				"Pennies" };
+
+		//if count is 0, do not show
+		for (int i = 0; i < denominations.length; i++) {
+			if (!(denominations[i] == 0)) {
+				System.out.println(denoms[i] + ":" + denominations[i]);
+			}
+		}
+
 	}
 }
